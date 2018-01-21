5479f9b852cda054d3e0cb8547de5154910d8a20
Marcus Larsson <marcuslarsson_92@hotmail.com>
2018-01-21 21:47:51 +0100

    added reallocate to cfx::mallocator

--- cfx/allocator/mallocator.h
+++ cfx/allocator/mallocator.h
@@ -3,21 +3,34 @@
 #ifndef CFX_ALLOCATOR_MALLOCATOR_H_
 #define CFX_ALLOCATOR_MALLOCATOR_H_
 
+#include <cstddef>
 #include <cstdlib>
-#include "block.h"
+
+#include "cfx/allocator/block.h"
 
 namespace cfx {
 
 class mallocator {
  public:
-    block allocate(size_t size) noexcept {
+    cfx::block allocate(size_t size) noexcept {
 	if (auto start = static_cast<std::byte*>(::malloc(size)); start != nullptr)
 	    return { start, start + size };
 
 	return { nullptr, nullptr };
     }
 
-    void deallocate(block& blk) noexcept {
+    bool reallocate(cfx::block& blk, size_t new_size) noexcept {
+	if (auto new_start = ::realloc(blk.start, new_size); new_start != nullptr) {
+	    blk.start = static_cast<std::byte*>(new_start);
+	    blk.end = blk.start + new_size;
+
+	    return true;
+	}
+
+	return false;
+    }
+
+    void deallocate(cfx::block& blk) noexcept {
 	::free(blk.start);
 	blk.reset();
     }
