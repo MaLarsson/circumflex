804a3693308cdb8630ac29797e4a1347c94cf886
Marcus Larsson <marcuslarsson_92@hotmail.com>
2018-01-21 21:46:44 +0100

    added aliases for cfx::vector

--- cfx/container/vector.h
+++ cfx/container/vector.h
@@ -10,8 +10,19 @@ namespace cfx {
 template <typename T, typename A = cfx::mallocator>
 class vector {
  public:
+    // Aliases
     using value_type = T;
     using allocator_type = A;
+    using pointer = value_type*;
+    using const_pointer = const value_type*;
+    using reference = value_type&;
+    using const_reference = const value_type&;
+    using size_type = size_t;
+    using difference_type = std::ptrdiff_t;
+    using iterator = pointer;
+    using const_iterator = const_pointer;
+    using reverse_iterator = std::reverse_iterator<iterator>;
+    using const_reverse_iterator = std::reverse_iterator<const_iterator>;
 
     // Constructors
     vector() noexcept(noexcept(allocator_type()))
