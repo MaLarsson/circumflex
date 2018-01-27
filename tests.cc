16731f568e04b448ae157175c8a71b6dd5ac5004
Marcus Larsson <marcuslarsson_92@hotmail.com>
2018-01-28 00:19:05 +0100

    added tests

--- tests.cc
+++ tests.cc
@@ -3,14 +3,22 @@
 
 int main()
 {
-    cfx::inlined_vector<int> v;
+    cfx::inlined_vector<int> v{4, 5};
+
+    std::cout << "empty: " << v.empty() << '\n';
 
     v.push_back(1);
     v.push_back(2);
     v.push_back(3);
 
+    std::cout << "empty: " << v.empty() << '\n';
+    std::cout << "size: " << v.size() << '\n';
+    std::cout << "capacity: " << v.capacity() << '\n';
+
     for (auto& x : v)
-	std::cout << x << '\n';
+	std::cout << x << ' ';
+
+    std::cout << '\n' << v.at(1) << '\n';
 
     return 0;
 }
